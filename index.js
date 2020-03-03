7aacb3b97b44a010fec8c826b6b5a8def8aa5fc5
Jinki Jung <your.jinki.jung@gmail.com>
2020-03-04 00:24:14 +0100

    addition of json schema

---
+++ index.js
@@ -0,0 +1,11 @@
+// grab the packages we need
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 4000;
+
+//var fileUploader = require('./js/fileUploader/fileUploader')(app);
+//var router = require('./js/router/router.js')(app);
+
+app.use(express.static('./'));
+app.listen(port);
+console.log('PaperMining: web server running on 4000...');
